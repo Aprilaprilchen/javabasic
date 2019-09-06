3d8767620f5a09e061755056a5e5a867caec65c7
陈俞彤 <yutong@gwu.edu>
2019-09-06 16:53:58 -0400

    queue

--- src/main/java/com.ascending.training.basic/dataStructure/QueueTest.java
+++ src/main/java/com.ascending.training.basic/dataStructure/QueueTest.java
@@ -1,5 +1,25 @@
 package com.ascending.training.basic.dataStructure;
 
-public class QueueTest {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class QueueTest {
+    public static Queue<Integer> number = new LinkedList<>();
+
+    // add elements
+    public static void main(){
+        number.add(8);
+        number.add(2);
+        number.add(6);
+        number.add(6);
+
+        System.out.println("The original queue" + number);
+
+        int n = number.remove();
+        System.out.println("the top number" + n);
+        System.out.println("the new queue" + number);
+
+        int m = number.peek();
+
+    }
 }

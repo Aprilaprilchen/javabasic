f964b7dbbbf05ff6f4b0a34590a15f736ddc5687
陈俞彤 <yutong@gwu.edu>
2019-09-06 16:59:41 -0400

    queue

--- src/main/java/com.ascending.training.basic/dataStructure/QueueTest.java
+++ src/main/java/com.ascending.training.basic/dataStructure/QueueTest.java
@@ -12,14 +12,20 @@ public class QueueTest {
         number.add(2);
         number.add(6);
         number.add(6);
+        System.out.println("the original queue: " + number);
 
-        System.out.println("The original queue" + number);
-
+        // remove the first value
         int n = number.remove();
-        System.out.println("the top number" + n);
-        System.out.println("the new queue" + number);
+        System.out.println("the top number: " + n);
+        System.out.println("the new queue: " + number);
 
+        // peek the first value
         int m = number.peek();
+        System.out.println("the top number: " + m);
+        System.out.println("the new queue: " + number);
 
+        // get the size
+        int size = number.size();
+        System.out.println("the size of the queue: " + size);
     }
 }

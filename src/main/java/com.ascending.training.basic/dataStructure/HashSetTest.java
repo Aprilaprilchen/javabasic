d43e1179e15e3f4ca06a72a6d75518c8d1e59ca4
陈俞彤 <yutong@gwu.edu>
2019-09-08 15:12:41 -0400

    hashset

--- src/main/java/com.ascending.training.basic/dataStructure/HashSetTest.java
+++ src/main/java/com.ascending.training.basic/dataStructure/HashSetTest.java
@@ -8,9 +8,14 @@ public class HashSetTest {
         Set<String> set = new HashSet<>();
         set.add("Cyt");
         set.add("tt");
-//        set.
+        set.remove("Cyt");
+        set.add("cyt");
         int i = set.size();
         int j = set.hashCode();
         System.out.println("The set is: "+set+" which size is: "+i+" , and hashcode is: "+j);
     }
 }
+/* Characters of Set:
+1. The elements cannot be duplicate;
+2. The elements do not have a certain order;
+ */

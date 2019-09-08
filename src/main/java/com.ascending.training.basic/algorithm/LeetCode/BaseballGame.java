ea3d80123fb7c004024fffab52597d15ae8a627e
陈俞彤 <yutong@gwu.edu>
2019-09-08 13:52:41 -0400

    stack

--- src/main/java/com.ascending.training.basic/algorithm/LeetCode/BaseballGame.java
+++ src/main/java/com.ascending.training.basic/algorithm/LeetCode/BaseballGame.java
@@ -42,8 +42,8 @@ public class BaseballGame {
 
     public static void main(String[] args){
         String[] ops = {"5", "2", "C", "D", "+"};
-        BaseballGame s = new BaseballGame();
-        int sum = s.calPoints(ops);
+        BaseballGame baseballGame = new BaseballGame();
+        int sum = baseballGame.calPoints(ops);
         System.out.println("sum number is: " + sum);
     }
 
